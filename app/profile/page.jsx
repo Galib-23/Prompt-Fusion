95219a6b1d274470fec729e5bad95657c5321514
Galib-23 <aagalib2323@gmail.com>
2024-04-18 01:01:53 +0600

    Delete functionlaity added

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -25,7 +25,21 @@ const MyProfile = () => {
   };
 
   const handleDelete = async (post) => {
-    
+    const hasConfirmed = confirm("Are you sure you want to delete the prompt?")
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: 'DELETE'
+        })
+
+        const filteredPosts = posts.filter((p) => p._id !== post._id);
+        setPosts(filteredPosts);
+
+      } catch (error) {
+        console.log('Error in deleting the prompt: ', error)
+        alert('Error in deleting the prompt')
+      }
+    }
   };
 
   if (!session) {
