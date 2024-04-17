64ae419d21949393d56320d63edd8d43a5a6cd85
Galib-23 <aagalib2323@gmail.com>
2024-04-17 23:59:41 +0600

    Get dynamic prompt api route init

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -7,11 +7,10 @@ export const GET = async (request) => {
     try {
         await connectToDB();
 
-        //populate also fethces the corresponding creator data from users document
         const prompts = await Prompt.find({}).populate('creator');
 
         return new Response(JSON.stringify(prompts), {status: 200})
     } catch (error) {
-        return new Response("Failed to fetch prompts", {status: 500})
+      
     }
 }
